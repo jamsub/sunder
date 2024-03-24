bc1c0c7a66a5c72ab2d7ad283de0310ede0f2e2b
jamsub <85641501+jamsub@users.noreply.github.com>
2024-03-24 11:04:10 -0500

    Update agent_deploy.ps1

--- agent_deploy.ps1
+++ agent_deploy.ps1
@@ -1,7 +1,10 @@
-# PowerShell script to deploy Elastic Agent 
-# YOU MUST CHANGE THE ENROLLMENT-TOKEN PER CUSTOMER
-mkdir C:\elastic-agent
-cd C:\elastic-agent
+# PowerShell script to deploy SUNDER SIEM Agent
+
+param (
+    [string]$enrollmentToken
+)
+mkdir C:\sunder
+cd C:\sunder
 
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.2-windows-x86_64.zip -OutFile elastic-agent-8.12.2-windows-x86_64.zip
@@ -9,4 +12,4 @@ Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agen
 Expand-Archive .\elastic-agent-8.12.2-windows-x86_64.zip -DestinationPath .
 cd elastic-agent-8.12.2-windows-x86_64
 
-.\elastic-agent.exe install -n --url=https://865728532f2e49c5bfd2f987753d5c01.fleet.us-central1.gcp.cloud.es.io:443 --enrollment-token=X3ZhamNJNEJkUTB0Q3RlVlc4Rno6ZVk0UXdaMTFSN2U1dTYyVHo0c3FIUQ== 
+.\elastic-agent.exe install -n --url=https://865728532f2e49c5bfd2f987753d5c01.fleet.us-central1.gcp.cloud.es.io:443 
