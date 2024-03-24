181750dde4826b01368d887eabcc30a5eddc1ca5
jamsub <85641501+jamsub@users.noreply.github.com>
2024-03-24 11:40:43 -0500

    Update agent_deploy.ps1

--- agent_deploy.ps1
+++ agent_deploy.ps1
@@ -12,4 +12,4 @@ Invoke-WebRequest -Uri https://artifacts.elastic.co/downloads/beats/elastic-agen
 Expand-Archive .\elastic-agent-8.12.2-windows-x86_64.zip -DestinationPath .
 cd elastic-agent-8.12.2-windows-x86_64
 
-.\elastic-agent.exe install -n --url=https://865728532f2e49c5bfd2f987753d5c01.fleet.us-central1.gcp.cloud.es.io:443 
+.\elastic-agent.exe install -n --url=https://865728532f2e49c5bfd2f987753d5c01.fleet.us-central1.gcp.cloud.es.io:443 --enrollmentToken=$enrollmentToken
