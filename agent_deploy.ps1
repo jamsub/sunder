adffe0a29314d1daacd4658970c13cc3177662b8
jamsub <85641501+jamsub@users.noreply.github.com>
2024-03-25 11:06:38 -0500

    Update agent_deploy.ps1

--- agent_deploy.ps1
+++ agent_deploy.ps1
@@ -5,6 +5,11 @@ param (
 )
 # Define the directory where you want to install Elastic Agent
 $installDirectory = "C:\SUNDERAgent"
+# Check if the installation directory already exists
+if (!(Test-Path -Path $installDirectory -PathType Container)) {
+    # If not exists, create the directory
+    New-Item -ItemType Directory -Path $installDirectory
+}
 mkdir $installDirectory
 cd $installDirectory
 $ProgressPreference = 'SilentlyContinue'
@@ -12,19 +17,27 @@ $ProgressPreference = 'SilentlyContinue'
 # Define download URL for Elastic Agent
 $downloadUrl = "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.12.2-windows-x86_64.zip"
 
-
+$zipFilePath = "$installDirectory\elastic-agent-8.12.2-windows-x86_64.zip"
 
 # Define your Elastic Stack URL
 $elasticStackURL = "https://865728532f2e49c5bfd2f987753d5c01.fleet.us-central1.gcp.cloud.es.io:443"
 
-# Download the Elastic Agent zip file
-Invoke-WebRequest -Uri $downloadUrl -OutFile "$installDirectory\elastic-agent-8.12.2-windows-x86_64.zip"
+# Check if the zip file already exists
+if (!(Test-Path -Path $zipFilePath)) {
+    # If not exists, download the Elastic Agent zip file
+    Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
+}
 
-# Expand the downloaded zip file
-Expand-Archive -Path "$installDirectory\elastic-agent-8.12.2-windows-x86_64.zip" -DestinationPath $installDirectory
+# Check if the zip file exists before proceeding
+if (Test-Path -Path $zipFilePath) {
+    # Expand the downloaded zip file
+    Expand-Archive -Path $zipFilePath -DestinationPath $installDirectory
 
-# Change directory to the Elastic Agent directory
-Set-Location -Path "$installDirectory\elastic-agent-8.12.2-windows-x86_64"
+    # Change directory to the Elastic Agent directory
+    Set-Location -Path "$installDirectory\elastic-agent-8.12.2-windows-x86_64"
 
-# Install the Elastic Agent
-.\elastic-agent.exe install -n --url=$elasticStackURL --enrollment-token=$enrollmentToken
+    # Install the Elastic Agent
+    .\elastic-agent.exe install --url=$elasticStackURL --enrollment-token=$enrollmentToken
+} else {
+    Write-Host "Failed to download Elastic Agent zip file."
+}
