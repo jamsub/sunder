0236e59c82ed117f59262324b52d323da1ba3d56
jamsub <85641501+jamsub@users.noreply.github.com>
2024-03-25 09:20:14 -0500

    Update copy pasta.ps1

--- copy pasta.ps1
+++ copy pasta.ps1
@@ -2,6 +2,7 @@
 Here is the link to the tokens "https://182683e7a48b41bdba7286d5ac2488e7.us-central1.gcp.cloud.es.io:9243/app/fleet/enrollment-tokens"
 
 powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+timeout /t 5
 powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/jamsub/sunder/main/agent_deploy.ps1 -OutFile agent_deploy.ps1
-timeout /t 20 
+timeout /t 15
 powershell .\agent_deploy.ps1 -enrollmentToken "CHANGE ME"
